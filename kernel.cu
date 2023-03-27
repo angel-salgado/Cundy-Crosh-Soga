7c97242c105f758b2676ee1777a8efb81d4bb021
angel-salgado <santiago.salgado@edu.uah.es>
2023-03-27 21:22:03 +0200

    Commit 1

--- kernel.cu
+++ kernel.cu
@@ -1,121 +1,115 @@
 ﻿
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
-
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
 #include <stdio.h>
+using namespace std;
+#include <string>
+#include <vector>
 
-cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
 
-__global__ void addKernel(int *c, const int *a, const int *b)
+
+
+__global__ void bajarBloquesKernel(int *c, const int *a, const int *b)
 {
     int i = threadIdx.x;
     c[i] = a[i] + b[i];
 }
 
-int main()
-{
-    const int arraySize = 5;
-    const int a[arraySize] = { 1, 2, 3, 4, 5 };
-    const int b[arraySize] = { 10, 20, 30, 40, 50 };
-    int c[arraySize] = { 0 };
+int* generar_matriz(int filas, int columnas, int maximo) {
 
-    // Add vectors in parallel.
-    cudaError_t cudaStatus = addWithCuda(c, a, b, arraySize);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addWithCuda failed!");
-        return 1;
+    int* tablero = new int[filas * columnas];
+
+    srand(time(NULL));
+
+    // Generar números aleatorios y almacenarlos en la matriz
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; j < columnas; j++) {
+            tablero[i * columnas + j] = rand() % maximo + 1;
+        }
     }
 
-    printf("{1,2,3,4,5} + {10,20,30,40,50} = {%d,%d,%d,%d,%d}\n",
-        c[0], c[1], c[2], c[3], c[4]);
+    return tablero;
+}
 
-    // cudaDeviceReset must be called before exiting in order for profiling and
-    // tracing tools such as Nsight and Visual Profiler to show complete traces.
-    cudaStatus = cudaDeviceReset();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceReset failed!");
-        return 1;
+// definición de la función
+string seleccionarConfiguracion() {
+    char ejecucion;
+    int nivel, filas, columnas;
+    string salida;
+    cout << "Introduce el modo de ejecucion: ";
+    cin >> ejecucion;
+    cout << "Introduce el nivel de dificultad (1 o 2): ";
+    cin >> nivel;
+    cout << "Introduce el numero de filas del tablero: ";
+    cin >> filas;
+    cout << "Introduce el nivel numero de columnas del tablero: ";
+    cin >> columnas;
+    salida = string(1, ejecucion) + to_string(nivel) + to_string(filas) + to_string(columnas);
+    return salida;
+}
+
+void print_matriz(int* matriz, int filas, int columnas) {
+    for (int i = 0; i < (filas + 1); i++) {
+        for (int j = 0; j < (columnas + 1); j++) {
+            if (j == 0) {
+                printf("%d ", i);
+
+            }
+            else if (i == 0) {
+                printf("%d ", j);
+
+            }
+            else {
+                printf("%d ", matriz[((i - 1) * columnas) + (j - 1)]);
+            }
+            //printf("%d ", matriz[i * columnas + j]);
+        }
+        printf("\n");
     }
+    printf("\n");
+}
+
+
+
+
+int main() {
+
+    int vidas, maximo, fila, columna = 5;
+
+    // llamada a la función y guardado del resultado en una variable
+    string salida = seleccionarConfiguracion();
+
+    // impresión de la salida por pantalla
+    cout << "La configuración elegida es: " << salida << endl;
+
+    if (salida[1] == '1') {
+        maximo = 4;
+    }
+    else {
+        maximo = 6;
+    }
+    //Se resta 48 al valor de salida[x] para cambiar el valor de ascii al número que nos interesa
+    int filas = salida[2] - 48;
+    int columnas = salida[3] - 48;
+
+    // Generar la matriz y mostrarla por pantalla
+    int* tablero = generar_matriz(filas, columnas, maximo);
+
+    print_matriz(tablero, filas, columnas);
+
+
+    /*while (vidas > 0) {
+        print_matriz(tablero, filas, columnas);
+        printf("Haga la fila seleccionada")
+        
+
+    }*/
 
     return 0;
 }
 
-// Helper function for using CUDA to add vectors in parallel.
-cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size)
-{
-    int *dev_a = 0;
-    int *dev_b = 0;
-    int *dev_c = 0;
-    cudaError_t cudaStatus;
 
-    // Choose which GPU to run on, change this on a multi-GPU system.
-    cudaStatus = cudaSetDevice(0);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
-        goto Error;
-    }
 
-    // Allocate GPU buffers for three vectors (two input, one output)    .
-    cudaStatus = cudaMalloc((void**)&dev_c, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMalloc((void**)&dev_a, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMalloc((void**)&dev_b, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    // Copy input vectors from host memory to GPU buffers.
-    cudaStatus = cudaMemcpy(dev_a, a, size * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMemcpy(dev_b, b, size * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-    // Launch a kernel on the GPU with one thread for each element.
-    addKernel<<<1, size>>>(dev_c, dev_a, dev_b);
-
-    // Check for any errors launching the kernel
-    cudaStatus = cudaGetLastError();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
-        goto Error;
-    }
-    
-    // cudaDeviceSynchronize waits for the kernel to finish, and returns
-    // any errors encountered during the launch.
-    cudaStatus = cudaDeviceSynchronize();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
-        goto Error;
-    }
-
-    // Copy output vector from GPU buffer to host memory.
-    cudaStatus = cudaMemcpy(c, dev_c, size * sizeof(int), cudaMemcpyDeviceToHost);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-Error:
-    cudaFree(dev_c);
-    cudaFree(dev_a);
-    cudaFree(dev_b);
-    
-    return cudaStatus;
-}
